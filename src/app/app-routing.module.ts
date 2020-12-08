bb4f361490c92b8fa30929e4d8ce3284866f38f0
lucas.ramos <lucas.ramos@sonda.com>
2020-12-08 11:23:47 -0300

    Criando o primeiro component do primeNg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TabViewModule } from 'primeng/tabview';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+    TabViewModule
+  ],
+  exports: [
+    RouterModule,
+    TabViewModule
+  ]
 })
 export class AppRoutingModule { }
